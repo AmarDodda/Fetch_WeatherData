3ae6efa9b3e9be0ec98b11c78af56dbb2fe9eaf8
AmarDodda <amardodda23@gmail.com>
2024-04-08 11:51:32 +0530

    Fetched the Weather data(temperature)

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
+// async function fetchDataAndDisplay() {
+//     try {
+//         const response = await fetch("https://restcountries.com/v3.1/all");
+//         const data1 = await response.json();
+//         foo(data1);
+//     } catch (error) {
+//         console.log("Error fetching data:", error);
+//     }
+// }
+
+// async function foo(data1) {
+//     const container = document.createElement("div");
+//     container.className = "container";
+    
+//     const row = document.createElement("div");
+//     row.className = "row row-cols-1 row-cols-md-3 gx-4"; 
+    
+//     data1.forEach(country => {
+//         const col = document.createElement("div");
+//         col.className = "col mb-4"; 
+//         col.innerHTML = `<div class="card border-primary h-100">
+//             <div class="card-header">${country.name.common}</div>
+//             <div class="card-body text-primary d-flex flex-column">
+//                 <img src="${country.flags.svg}" class="img-fluid mb-2" style="max-height: 150px; width: auto;" alt="Flag">
+//                 <h5 class="card-title">Capital: ${country.capital}</h5>
+//                 <p class="card-text">Region: ${country.region}</p>
+//                 <p class="card-text">Country Code: ${country.cca2}</p>
+//                 <p class="card-text">Population: ${country.population}</p>
+//                 <button class="btn btn-primary mt-auto" onclick="getWeather('${country.name.common}')">Click for Weather Data</button>
+//             </div>
+//         </div>`;
+//         row.appendChild(col);
+//     });
+    
+//     container.appendChild(row);
+//     document.body.appendChild(container);
+    
+// }
+
+// fetchDataAndDisplay();
+
+
+
 async function fetchDataAndDisplay() {
     try {
         const response = await fetch("https://restcountries.com/v3.1/all");
@@ -9,36 +52,44 @@ async function fetchDataAndDisplay() {
 }
 
 async function foo(data1) {
-    var container = document.createElement("div");
+    const container = document.createElement("div");
     container.className = "container";
 
-    var row;
-    for (var i = 0; i < data1.length; i++) {
-        if (i % 3 === 0) {
-            row = document.createElement("div");
-            row.className = "row mb-4";
-        }
-        var col = document.createElement("div");
-        col.className = "col-md-4";
-        col.innerHTML = `<div class="card border-primary mb-3 h-100" style="max-width: 18rem;">
-            <div class="card-header">${data1[i].name.common}</div>
+    const row = document.createElement("div");
+    row.className = "row row-cols-1 row-cols-md-3 gx-4";
+
+    data1.forEach(country => {
+        const col = document.createElement("div");
+        col.className = "col mb-4";
+        col.innerHTML = `<div class="card border-primary h-100">
+            <div class="card-header">${country.name.common}</div>
             <div class="card-body text-primary d-flex flex-column">
-              <img src="${data1[i].flags.svg}" class="img-fluid mb-2" style="max-height: 150px; width: auto;" alt="Flag">
-              <h5 class="card-title">Capital: ${data1[i].capital}</h5>
-              <p class="card-text">Region: ${data1[i].region}</p>
-              <p class="card-text">Country Code: ${data1[i].cca2}</p>
-              <p class="card-text">Population: ${data1[i].population}</p>
-              <button class="btn btn-primary mt-auto" onclick="getWeather('${data1[i].name.common}')">Click for Weather Data</button>
+                <img src="${country.flags.svg}" class="img-fluid mb-2" style="max-height: 150px; width: auto;" alt="Flag">
+                <h5 class="card-title">Capital: ${country.capital}</h5>
+                <p class="card-text">Region: ${country.region}</p>
+                <p class="card-text">Country Code: ${country.cca2}</p>
+                <p class="card-text">Population: ${country.population}</p>
+                <p class="card-text">Latitude: ${country.latlng[0]}</p>
+                <p class="card-text">Longitude: ${country.latlng[1]}</p>
+                <button class="btn btn-primary mt-auto" onclick="getWeather('${country.name.common}', ${country.latlng[0]}, ${country.latlng[1]})">Click for Weather Data</button>
             </div>
-          </div>`;
-        row.append(col);
+        </div>`;
+        row.appendChild(col);
+    });
 
-        if ((i + 1) % 3 === 0 || i === data1.length - 1) {
-            container.append(row);
-        }
+    container.appendChild(row);
+    document.body.appendChild(container);
+}
+
+async function getWeather(countryName, lat, lon) {
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=23f04cb3fe3a2337ea1dcd4a5cd1718e`);
+        const weatherData = await response.json();
+        const temperature = weatherData.main.temp;
+        alert(`Temperature in ${countryName}: ${temperature} Kelvin`);
+    } catch (error) {
+        console.error('Error fetching weather data:', error);
     }
-
-    document.body.append(container);
 }
 
 fetchDataAndDisplay();
